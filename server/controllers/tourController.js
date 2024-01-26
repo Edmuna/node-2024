62db096e0b3353ed8e286bdf92aa3f904c7e72fc
Edmuna <ereda.hr@gmail.com>
2024-01-26 10:05:55 +0100

    better api features

--- server/controllers/tourController.js
+++ server/controllers/tourController.js
@@ -1,69 +1,30 @@
 const Tour = require("../models/tourModel")
+const APIFeatures = require("../utils/apifeatures")
 
 exports.getAllTours = async (req, res) => {
-
     try {
-        // BUILD QUERY
-        const queryObj = { ...req.query }
-        const excludedFields = ["page", "sort", "limit", "fields"]
-        excludedFields.forEach(el => delete (queryObj[el]))
-        console.log(req.query)
-
-        // ADVANCED FILTERING
-        let queryStr = JSON.stringify(queryObj)
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
-        console.log(JSON.parse(queryStr))
-
-        let query = Tour.find(JSON.parse(queryStr))
-
-        // SORTING
-        if (req.query.sort) {
-            const sortBy = req.query.sort.split(",").join(" ")
-            query = query.sort(sortBy)
-        } else {
-            query = query.sort("-createdAt")
-        }
-
-        // FIELD LIMITING
-        if (req.query.fields) {
-            const fields = req.query.fields.split(",").join(" ")
-            query = query.select(fields)
-        } else {
-            query = query.select("-__v")
-        }
-
-        // PAGINATION
-
-        const page = req.query.page * 1 || 1
-        const limit = req.query.limit * 1 || 100
-        const skip = (page - 1) * limit
-
-        query = query.skip(skip).limit(limit)
-
-        if (req.query.page) {
-            const numTours = await Tour.countDocuments()
-            if (skip >= numTours) throw new Error("This page does not exist")
-        }
-
         // EXECUTE QUERY
-        const tours = await query
+        const features = new APIFeatures(Tour.find(), req.query)
+            .filter()
+            .sort()
+            .limitFields()
+            .paginate();
+        const tours = await features.query;
 
-
+        // SEND RESPONSE
         res.status(200).json({
             status: 'success',
-            requestedAt: req.requestTime,
             results: tours.length,
             data: {
                 tours
             }
         });
     } catch (err) {
-        res.status(400).json({
-            status: "failed",
+        res.status(404).json({
+            status: 'fail',
             message: err
-        })
+        });
     }
-
 };
 
 exports.getTour = async (req, res) => {
